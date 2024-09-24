aab6f40fdbc7ccf1c1ca08a8cdc1f072fd42546d
unknown <dmitro.beregovij20@gmail.com>
2024-09-24 20:26:04 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+import sys
+import math
+a=float(input("Введіть значення a: "))
+b=float(input("Введіть значення b: "))
+c=float(input("Введіть значення c: "))
+V=(a*b*c)
+print(f"V= {V}")
+S=2*(a*b+a*c+b*c)
+print(f'S= {S}')
